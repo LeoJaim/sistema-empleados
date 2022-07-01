fb5347ec454c20d672ab024976baa9c3251a5a51
Leojaim <leonardo.jaimovich@gmail.com>
2022-07-01 00:40:45 -0300

    Alta-consulta-empleados

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from sqlite3 import Cursor
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -8,9 +9,39 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Minolta10*'
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    
+    sql = "SELECT nombre,correo,foto FROM empleados"
+    cursor.execute(sql)
+    
+    return render_template('empleados/index.html', empleados=cursor)
+
+@app.route('/alta_emp')
+def alta_emp():
+    return render_template('empleados/create.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    _nombre = request.form['nombre']
+    _correo = request.form['correo']
+    _foto = request.form['foto']
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    sql = "INSERT INTO empleados (id,nombre,correo,foto) VALUES (NULL,%s,%s,%s)"
+    cursor.execute(sql, (_nombre,_correo,_foto))
+    conn.commit()
+    return redirect('/')    
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-
